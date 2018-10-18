53ddc3ad1624fd324d01a81ee17b2541043c2c7d
Vyper248 <vyper375@hotmail.com>
2018-10-18 14:34:34 +0100

    moved getting post details to app, and added conditions for when the user moves back and forward in the browser. Too messy.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class Page extends Component {
         this.state = {
             sub: 'Home',
             heading: '',
-            posts: []
+            posts: [],
+            postDetails: {title: '', body: ''}
         };
     }
     
@@ -21,7 +22,7 @@ class Page extends Component {
                 <Switch>
                     <Route exact path="/" render={props => <PostList {...props} posts={this.state.posts}/>} />
                     <Route exact path="/:sub" render={props => <PostList {...props} posts={this.state.posts}/>} />
-                    <Route exact path="/:sub/:post" component={Post} />
+                    <Route exact path="/:sub/:post" render={props => <Post {...props} postDetails={this.state.postDetails} />} />
                 </Switch>
             </div>
         );
@@ -42,19 +43,70 @@ class Page extends Component {
                 };
             });
             
+            console.log('setting post array for ',sub);
             this.setState({posts});
         }
     };
     
+    getPostDetails = async (url) => {
+        console.log('getting post details');
+        try {
+            let response = await fetch('https://www.reddit.com/r/'+url+'.json');
+            let data = await response.json();
+            
+            if (data.error){
+                this.setState({title: 'Not Found', body: ''});
+            } else {
+                let {title, selftext_html} = data[0].data.children[0].data;
+                //if this exists, replace &lt etc with proper symbols, otherwise set to empty string
+                selftext_html ? selftext_html = selftext_html.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;#39;/g,"'") : selftext_html = '';
+                this.setState({postDetails: {title, body: selftext_html}});
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
     componentDidUpdate(prevProps){
+        console.log('update');
         if (prevProps.location.pathname !== this.props.location.pathname){
+            console.log('location different');
             //check if needs to get new posts
-            if (prevProps.location.pathname.match(/\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+/) && this.props.location.pathname.match(/\/[a-zA-Z0-9]+/)) {
+            let matches = this.props.location.pathname.match(/\//g);
+            matches = matches ? matches.length : 0;
+            let prevMatches = prevProps.location.pathname.match(/\//g);
+            prevMatches = prevMatches ? prevMatches.length : 0;
+
+            if (prevMatches === 2 && matches === 1) {
                 //going from post to sub, so don't need to update, already have array
-            } else if (prevProps.location.pathname.match(/\/[a-zA-Z0-9]+/) && this.props.location.pathname.match(/\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+/)) {
+                let oldSub = prevProps.location.pathname.match(/\/([a-zA-Z0-9]+)\//)[1];
+                let url = this.props.location.pathname.replace('/','');
+                if (url !== oldSub){
+                    console.log('post to different sub, need updating');
+                    this.setState({posts: []});
+                    this.getPostList(url);
+                } else if (this.state.posts.length === 0) {
+                    console.log('post to sub, getting new array because started with empty');
+                    this.getPostList(url);
+                } else {
+                    console.log('post to sub, no updated needed');
+                }
+            } else if (prevMatches === 1 && matches === 2) {
                 //going from sub to post, so don't need to update
+                console.log('sub to post');
+                let oldSub = prevProps.location.pathname.match(/\/([a-zA-Z0-9]+)/)[1];
+                let newSub = this.props.location.pathname.match(/\/([a-zA-Z0-9]+)\//)[1];
+                let url = this.props.location.pathname.replace('/','');
+                if (newSub !== oldSub){
+                    console.log('sub to different subs post, need updating', url, oldSub);
+                    this.getPostList(url.match(/([a-zA-Z0-9]+)\//)[1]);
+                }
+                
+                this.setState({postDetails: {title: '', body: ''}});
+                this.getPostDetails(url);
             } else {
                 //going from one sub to another sub, so do need to update
+                console.log('sub to sub');
                 this.setState({posts: []});
                 this.getPostList(this.props.location.pathname.replace('/',''));
             }
@@ -63,7 +115,17 @@ class Page extends Component {
     
     componentDidMount(){
         let url = this.props.location.pathname.replace('/','');
-        this.getPostList(url);
+        //check initial url and fetch required data
+        let matches = this.props.location.pathname.match(/\//g);
+        matches = matches ? matches.length : 0;
+        if (matches === 1){
+            //on sub, so get post list
+            this.getPostList(url);
+        } else if (matches === 2){
+            //on post, so get post list and post details
+            this.getPostList(url);
+            this.getPostDetails(url);
+        }
     }
 }
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,68 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Post extends Component {
-    constructor({match}){
-        super();
-        this.match = match;
-        this.state = {
-            title: '',
-            body: '',
-        };
-    }
-    
-    render(){
-        return (
-            <div>
-                {
-                    this.state.title.length === 0 ? <h1>Loading...</h1> : (
-                        <div>
-                            <h1>{this.state.title}</h1>
-                            <div dangerouslySetInnerHTML={{ __html: this.state.body }}></div>
-                        </div>
-                    )
-                }
-            </div>
-        );
-    }
-    
-    componentDidMount(){
-        let {sub, post} = this.match.params;
-        this.getPostDetails(`${sub}/comments/${post}.json`);
-    }
-    
-    getPostDetails = async (url) => {
-        try {
-            let response = await fetch('https://www.reddit.com/r/'+url);
-            let data = await response.json();
-            
-            if (data.error){
-                this.setState({title: 'Not Found', body: ''});
-            } else {
-                let {title, selftext_html} = data[0].data.children[0].data;
-                //if this exists, replace &lt etc with proper symbols, otherwise set to empty string
-                selftext_html ? selftext_html = selftext_html.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;#39;/g,"'") : selftext_html = '';
-                this.setState({title, body: selftext_html});
+const Post = (props) => {
+    let {title, body} = props.postDetails;
+    return (
+        <div>
+            {
+                title.length === 0 ? <h1>Loading...</h1> : (
+                    <div>
+                        <h1>{title}</h1>
+                        <div dangerouslySetInnerHTML={{ __html: body }}></div>
+                    </div>
+                )
             }
-        } catch (error) {
-            console.log(error);
-        }
-            
-    };
+        </div>
+    );
 }
-
-// const Post = ({title, body}) => {
-//     return (
-//         <div>
-//             {
-//                 title.length === 0 ? <h1>Loading...</h1> : (
-//                     <div>
-//                         <h1>{title}</h1>
-//                         <div dangerouslySetInnerHTML={{ __html: body }}></div>
-//                     </div>
-//                 )
-//             }
-//         </div>
-//     );
-// }
 
 export default Post;
