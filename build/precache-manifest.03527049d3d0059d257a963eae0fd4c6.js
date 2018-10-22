63522dcb2c99efe70101ad2a1b441ee86cf3ced9
Vyper248 <vyper375@hotmail.com>
2018-10-22 13:25:48 +0100

    added build folder for display on github pages

---
+++ build/precache-manifest.03527049d3d0059d257a963eae0fd4c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5eead482eeb7b377ac7",
+    "url": "/new-reddit/build/static/js/runtime~main.c5eead48.js"
+  },
+  {
+    "revision": "3c0fe67fe64867ab4b2e",
+    "url": "/new-reddit/build/static/js/main.3c0fe67f.chunk.js"
+  },
+  {
+    "revision": "29661820fe3728f057e9",
+    "url": "/new-reddit/build/static/js/1.29661820.chunk.js"
+  },
+  {
+    "revision": "3c0fe67fe64867ab4b2e",
+    "url": "/new-reddit/build/static/css/main.847ba1b3.chunk.css"
+  },
+  {
+    "revision": "0637caa0f5160630a3ac2fefc64bcf32",
+    "url": "/new-reddit/build/index.html"
+  }
+];
