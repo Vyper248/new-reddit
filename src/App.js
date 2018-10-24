db1f096cf3e8aaccac79d301612a0a73c8bcb464
Vyper248 <vyper375@hotmail.com>
2018-10-24 10:23:01 +0100

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import SubList from './components/SubList';
 import PostList from './components/PostList';
 import Post from './components/Post';
