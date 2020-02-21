c49800187df58dbe34f57fc22110e4049757dc36
Vyper248 <vyper375@hotmail.com>
2020-02-21 13:03:54 +0000

    Updates

---
+++ precache-manifest.3c538cc5c4da9b45de9a56f3923b2798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99df07c6e130bed664584bd0522bc5f6",
+    "url": "/new-reddit/index.html"
+  },
+  {
+    "revision": "f7038c375ca059a602ea",
+    "url": "/new-reddit/static/css/main.c51e20a2.chunk.css"
+  },
+  {
+    "revision": "ba7879c75bdb87e93528",
+    "url": "/new-reddit/static/js/2.6b2a7a08.chunk.js"
+  },
+  {
+    "revision": "8e206c6708675092ad76215856e3fa46",
+    "url": "/new-reddit/static/js/2.6b2a7a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7038c375ca059a602ea",
+    "url": "/new-reddit/static/js/main.0ad3c9dc.chunk.js"
+  },
+  {
+    "revision": "7eaaa34a03b3ecab3fa7",
+    "url": "/new-reddit/static/js/runtime-main.2696e697.js"
+  }
+]);
