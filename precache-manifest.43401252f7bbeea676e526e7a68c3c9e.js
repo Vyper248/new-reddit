56662e5787882b68265f3046da20cd139ba11af9
Vyper248 <vyper375@hotmail.com>
2021-07-01 19:34:45 +0100

    Updates

--- precache-manifest.43401252f7bbeea676e526e7a68c3c9e.js
+++ precache-manifest.43401252f7bbeea676e526e7a68c3c9e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89a4e0e9bbf399f0e82a758b38e6fa29",
+    "revision": "c0140dd5833bf4c38b0f73d5040a2c90",
     "url": "/new-reddit/index.html"
   },
   {
-    "revision": "064a1aa2d52b2e8cb61c",
+    "revision": "b7cd7e191b8c0b073f1f",
     "url": "/new-reddit/static/css/main.1db71b39.chunk.css"
   },
   {
-    "revision": "58846fb4ff944f04feb9",
-    "url": "/new-reddit/static/js/2.ced596e7.chunk.js"
+    "revision": "95525ab5d01b0c076193",
+    "url": "/new-reddit/static/js/2.034fe753.chunk.js"
   },
   {
     "revision": "a6faf8bc2b4a2d8e02a3a36d122ea884",
-    "url": "/new-reddit/static/js/2.ced596e7.chunk.js.LICENSE.txt"
+    "url": "/new-reddit/static/js/2.034fe753.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "064a1aa2d52b2e8cb61c",
-    "url": "/new-reddit/static/js/main.489f3fe9.chunk.js"
+    "revision": "b7cd7e191b8c0b073f1f",
+    "url": "/new-reddit/static/js/main.eb7d1c56.chunk.js"
   },
   {
     "revision": "7eaaa34a03b3ecab3fa7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/new-reddit/precache-manifest.f18ed8800a742a03c9625e827c4635b8.js"
+  "/new-reddit/precache-manifest.43401252f7bbeea676e526e7a68c3c9e.js"
 );
 
 self.addEventListener('message', (event) => {
